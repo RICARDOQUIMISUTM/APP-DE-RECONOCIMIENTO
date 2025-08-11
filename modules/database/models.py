b75e13f986bd9c027c075b502c95de109c79c5e1
RICARDOQUIMISUTM <rquimis4742@utm.edu.ec>
2025-08-11 13:52:17 -0500

    Vaciando repositorio

--- modules/database/models.py
+++
@@ -1,6 +0,0 @@
-# modelos de datos sencillos (puede ampliarse)
-class User:
-    def __init__(self, user_id=None, name=None, created_at=None):
-        self.user_id = user_id
-        self.name = name
-        self.created_at = created_at
