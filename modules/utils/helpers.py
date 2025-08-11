5c5c4f86c4f8dfe5605aa823afadbb92f429a210
RICARDOQUIMISUTM <rquimis4742@utm.edu.ec>
2025-08-11 13:59:34 -0500

    Vaciando repositorio

--- modules/utils/helpers.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-def crop_face_from_frame(frame, face_rect):
-    # face_rect = (x,y,w,h)
-    x,y,w,h = face_rect
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    roi = gray[y:y+h, x:x+w]
-    return roi
